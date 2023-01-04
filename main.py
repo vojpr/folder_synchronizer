efb245e3d1af6d9db36f1099f103c6219a49f36a
vojpr <p.prusavojta@gmail.com>
2023-01-04 13:40:25 +0100

    Add main() function

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from folder_synchronizer import FolderSynchronizer
 import argparse
 
-arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument(dest="source", type=str, help="This is the source folder path")
-arg_parser.add_argument(dest="replica", type=str, help="This is the replica folder path")
-arg_parser.add_argument(dest="interval", type=int, help="This is the sync interval in whole minutes")
-arg_parser.add_argument(dest="logfolder", type=str, help="This is a path to the folder, where log file is/will be created")
-args = arg_parser.parse_args()
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(dest="source", type=str, help="This is the source folder path")
+    arg_parser.add_argument(dest="replica", type=str, help="This is the replica folder path")
+    arg_parser.add_argument(dest="interval", type=int, help="This is the sync interval in whole minutes")
+    arg_parser.add_argument(dest="logfolder", type=str, help="This is a path to the folder, where log file is/will be created")
+    args = arg_parser.parse_args()
 
-synchronizer = FolderSynchronizer(args.source, args.replica, args.interval, args.logfolder)
-synchronizer.run_synchronization()
+    synchronizer = FolderSynchronizer(args.source, args.replica, args.interval, args.logfolder)
+    synchronizer.run_synchronization()
+    
+    
+if __name__ == "__main__":
+    main()
