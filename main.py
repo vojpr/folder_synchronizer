782460cfb194b25143188719615015d1b1a54a1f
vojpr <p.prusavojta@gmail.com>
2023-02-07 12:30:40 +0100

    Update structure

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from folder_synchronizer import FolderSynchronizer
 import argparse
 
 
-def main():
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument(dest="source", type=str, help="This is the source folder path")
-    arg_parser.add_argument(dest="replica", type=str, help="This is the replica folder path")
-    arg_parser.add_argument(dest="interval", type=int, help="This is the sync interval in whole minutes")
-    arg_parser.add_argument(dest="logfolder", type=str, help="This is a path to the folder, where log file is/will be created")
-    args = arg_parser.parse_args()
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument(dest="source", type=str, help="This is the source folder path")
+arg_parser.add_argument(dest="replica", type=str, help="This is the replica folder path")
+arg_parser.add_argument(dest="interval", type=int, help="This is the sync interval in whole minutes")
+arg_parser.add_argument(dest="logfolder", type=str, help="This is a path to the folder, where log file is/will be created")
+args = arg_parser.parse_args()
 
-    synchronizer = FolderSynchronizer(args.source, args.replica, args.interval, args.logfolder)
+synchronizer = FolderSynchronizer(args.source, args.replica, args.interval, args.logfolder)
+
+
+def main():
     synchronizer.run_synchronization()
     
     
